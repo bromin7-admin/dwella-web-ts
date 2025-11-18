53427954aa8ad1bccf5fb8490fc2c7b02ba448bb
bromin7-admin <sergeyb@bromin7.com>
2025-11-18 18:24:55 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { CopilotChat } from "./components/CopilotChat";
-import { AuthModal } from "./components/AuthModal";
-import { FeedbackButtons } from "./components/FeedbackButtons";
-import { Suggestions } from "./components/Suggestions";
+import { Routes, Route } from "react-router-dom";
+import CopilotChat from "./components/CopilotChat";
+import AuthModal from "./components/AuthModal";
+import FeedbackButtons from "./components/FeedbackButtons";
+import Suggestions from "./components/Suggestions";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CopilotChat />} />
-        <Route path="/auth" element={<AuthModal />} />
-        <Route path="/suggestions" element={<Suggestions />} />
-        <Route path="/feedback" element={<FeedbackButtons />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<CopilotChat />} />
+      <Route path="/auth" element={<AuthModal />} />
+      <Route path="/suggestions" element={<Suggestions />} />
+      <Route path="/feedback" element={<FeedbackButtons />} />
+    </Routes>
   );
 }
