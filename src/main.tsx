a0385dbd54d2b64869b3a45144a75927bad98c7a
bromin7-admin <sergeyb@bromin7.com>
2025-11-18 17:37:50 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// Global styles
+import "./styles/landing.css";
+import "./styles/auth.css";
+import "./styles/copilot.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
