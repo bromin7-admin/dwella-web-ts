fa9bf3660d6f6f51d95295382a5cfd613ba219c4
bromin7-admin <sergeyb@bromin7.com>
2025-11-18 19:13:47 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.stage.dwella.co/v2",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: true,
+      },
+    },
+  },
 });
